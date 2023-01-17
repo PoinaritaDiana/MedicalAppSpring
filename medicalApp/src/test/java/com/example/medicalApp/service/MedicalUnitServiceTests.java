253a59d1adfa6ce6ed1664e3b447a663aa8afe38
Diana Poinarita <61639111+PoinaritaDiana@users.noreply.github.com>
2023-01-17 02:05:30 +0200

    Add MedicalUnitServiceTests

--- medicalApp/src/test/java/com/example/medicalApp/service/MedicalUnitServiceTests.java
+++ medicalApp/src/test/java/com/example/medicalApp/service/MedicalUnitServiceTests.java
@@ -1,5 +1,6 @@
 package com.example.medicalApp.service;
 
+import com.example.medicalApp.model.MedicalUnit;
 import com.example.medicalApp.repository.MedicalUnitRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class MedicalUnitServiceTests {
@@ -19,5 +28,42 @@ public class MedicalUnitServiceTests {
     @Test
     @DisplayName("Adding new medical unit - happy flow")
     void addNewMedicalUnitHappyFlow() {
+        //arrange
+        MedicalUnit medicalUnit = buildMedicalUnitMock();
+        when(medicalUnitRepository.save(medicalUnit)).thenReturn(medicalUnit);
+
+        //act
+        MedicalUnit result = medicalUnitService.addNewMedicalUnit(medicalUnit);
+
+        //assert
+        assertNotNull(result);
+        assertEquals(medicalUnit.getName(), result.getName());
+        assertEquals(medicalUnit.getCity(), result.getCity());
+        assertEquals(medicalUnit.getPhone(), result.getPhone());
+    }
+
+
+    @Test
+    @DisplayName("Get all medical units from city - happy flow")
+    void getAllMedicalUnitsFromCityHappyFlow() {
+        //arrange
+        String city = "Ploiesti";
+        List<MedicalUnit> medicalUnitList = new ArrayList<>();
+        medicalUnitList.add(buildMedicalUnitMock());
+        when(medicalUnitRepository.findAllByCity(anyString())).thenReturn(medicalUnitList);
+
+        //act
+        List<MedicalUnit> result = medicalUnitService.getAllMedicalUnitsFromCity(city);
+
+        //assert
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        assertEquals(medicalUnitList.get(0).getName(), result.get(0).getName());
+        assertEquals(medicalUnitList.get(0).getCity(), result.get(0).getCity());
+        assertEquals(medicalUnitList.get(0).getPhone(), result.get(0).getPhone());
+    }
+
+    private MedicalUnit buildMedicalUnitMock() {
+        return new MedicalUnit("Ploiesti", "Medlife", "1111111111");
     }
 }
